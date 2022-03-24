fc896f22646d9a1ba7fa2309c3607d890ad42943
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-03-24 19:06:21 +0000

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count + 1)}>
-            sumar
-          </button>
-          <button type="button" onClick={() => {
-            if (count > 0) setCount(count - 1)
-          }}>
-            restar
-          </button>
-        </p>
-        <p>
-          { count }
-        </p>
-      </header>
     </div>
   )
 }
