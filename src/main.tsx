b466c8ac0fe84ab609a7bf240cfb4a445fd92a2a
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-04-23 18:59:42 +0100

    restart

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
-ReactDOM.render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
