3e7c2e6430bec10f0688719d9d74143ffb54f8d8
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-06-07 23:38:49 +0100

    variant flushed sortfilter

--- src/components/SortFilter/SortFilter.tsx
+++ src/components/SortFilter/SortFilter.tsx
@@ -5,7 +5,8 @@ const SortFilter = () => {
     <div className='flex items-center gap-3'>
       <Text>Ordenar por:</Text>
       <div className='w-[20%]'>
-        <Select placeholder='Valoraciones' onChange={(e) => console.log(e.target.value)}>
+        <Select placeholder='Valoraciones' variant='flushed' color='#0E141B' _placeholder={{ color: 'inherit' }} focusBorderColor='#0E141B' onChange={(e) => console.log(e.target.value)}>
+          {/* <Select placeholder='Elegir' variant='flushed' color='#0E141B' _placeholder={{ color: 'inherit' }} focusBorderColor='#0E141B' isRequired {...register('typeService')} className='cursor-pointer'> */}
           <option value='name'>Nombre</option>
           <option value='rate'>Valoraciones</option>
           <option value='type'>Tipo de trabajo</option>
